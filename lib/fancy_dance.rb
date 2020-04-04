b77cc06ad88ceff3e082bd9a2240d89f6f52f2ac
Adam Priest <sepone229@gmail.com>
2020-04-04 00:40:59 +0000

    Automatically backed up by Learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,21 @@
+module FancyDance
+  module InstanceMethods
+    def twirl
+      "I'm twirling!"
+    end
+    def jump
+      "Look how high I can jump!"
+    end
+    def pirouette
+      "I'm doing a pirouette."
+    end
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+  end
+  module Class Methods
+    def metadata
+      "this class produces objects that love to dance."
+    end
+  end
+end

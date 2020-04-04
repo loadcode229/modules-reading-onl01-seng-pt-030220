6735fa9ea67f3ae8a30c6efa1296f8744d739386
Adam Priest <sepone229@gmail.com>
2020-04-04 00:44:15 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,5 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  extend MetaDancing
 end
